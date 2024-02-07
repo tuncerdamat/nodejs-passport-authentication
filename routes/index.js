315fdeea6c357121d8df557cbde94b367cf88406
tuncerdamat <tuncer.damat@gmail.com>
2024-02-07 14:54:40 +0100

    Pass user name to the dashboard

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ const { ensureAuthenticated } = require('../config/auth')
 router.get('/', (req, res) => res.render('welcome'))
 
 // Dashboard 
-router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dashboard'))
+router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dashboard', {
+    name: req.user.name
+}))
 
 module.exports = router
